2e1018bf5d9e5930435b246d875874c82f602d71
Rob Thijssen <rthijssen@gmail.com>
2021-08-19 09:58:52 +0300

    extrinsics benchmark history

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import * as Realm from 'realm-web';
+import {
+  CartesianGrid,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis
+} from 'recharts';
+
+const realmApp = new Realm.App({ id: 'benchmark-vrsnt' });
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const [benchmarks, setBenchmarks] = useState({});
+  useEffect(() => {
+    realmApp.logIn(Realm.Credentials.anonymous())
+      .then(
+        async (user) => {
+          setUser(user);
+        })
+      .catch(error => { console.error('login failure', error); });
+  }, []);
+  useEffect(() => {
+    if (!!user) {
+      const client = user.mongoClient('mongodb-atlas');
+      const db = client.db('Manta-Network');
+      db.collection('Manta').find()
+        .then(all => all.reduce((aA, aI) => (
+          {
+            ...aA,
+            [aI.pallet]: all.filter(pfI => pfI.pallet === aI.pallet).reduce((pA, pI) => (
+              {
+                ...pA,
+                [pI.extrinsic]: all.filter(efI => efI.extrinsic === pI.extrinsic && efI.pallet === pI.pallet).map(x => ({
+                  time: x.actual.time,
+                  unit: x.actual.unit,
+                  repo: x.repo,
+                  sha: x.sha.slice(0, 7),
+                  observed: x.observed, // todo: replace with commit time, when available
+                }))
+              }
+            ), {})
+          }
+        ), {}))
+        .then(setBenchmarks);
+    }
+  }, [user]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {
+        Object.keys(benchmarks).map(pallet => (
+          <div key={pallet}>
+            <h2>{pallet}</h2>
+            {
+              Object.keys(benchmarks[pallet]).map(extrinsic => (
+                <Row key={extrinsic}>
+                  <h3>{extrinsic}</h3>
+                  <Col>
+                    <LineChart width={600} height={400} data={benchmarks[pallet][extrinsic]}>
+                      <Line type="monotone" dataKey="time" stroke="#8884d8" />
+                      <CartesianGrid stroke="#ccc" />
+                      <XAxis dataKey="sha" />
+                      <YAxis />
+                    </LineChart>
+                  </Col>
+                  <Col>
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>sha</th>
+                          <th>time</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {
+                          benchmarks[pallet][extrinsic].map(benchmark => (
+                            <tr key={benchmark.sha}>
+                              <td>
+                                <a href={`https://github.com/${benchmark.repo}/commit/${benchmark.sha}`}>
+                                  {benchmark.sha}
+                                </a>
+                              </td>
+                              <td>{benchmark.time} {benchmark.unit}</td>
+                            </tr>
+                          ))
+                        }
+                      </tbody>
+                    </Table>
+                  </Col>
+                </Row>
+              ))
+            }
+          </div>
+        ))
+      }
+    </Container>
   );
 }
 
 export default App;
+
+//const filteredBenchmarks = benchmarks.map(x => ({ time: x.actual.time, sha: x.sha.slice(0, 7) }));
+/*
+*/
